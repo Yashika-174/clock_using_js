ce9fbf9978caf1b8085fffe72bd6ddb07256c971
Yashika Aggarwal <yashikaaggarwal0107@gmail.com>
2023-12-21 11:08:29 +0530

    Changes to be committed: 	modified:   clock.css 	modified:   clock.js 	modified:   index.html

--- clock.js
+++ clock.js
@@ -1,11 +1,28 @@
+let a = new Date();
+let hours = a.getHours();
+let minutes = a.getMinutes();
+let seconds = a.getSeconds();
+let ampm_value = 'AM';
+let day_name = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday']
+let days = a.getDay()
+let date_value = a.toLocaleDateString();
 
 
 let fun = setInterval(function () {
-    let a = new Date()
-    let hours = a.getHours();
-    let minutes = a.getMinutes();
-    let seconds = a.getSeconds();
-    value1.innerHTML = `${hours}`;
-    value2.innerHTML = `${minutes}`;
-    value3.innerHTML = `${seconds}`;
+    a = new Date();
+    hours = a.getHours();
+    minutes = a.getMinutes();
+    seconds = a.getSeconds();
+    days = a.getDay();
+    date_value = a.toLocaleDateString("en-GB");
+    if (hours > 12) {
+        ampm_value = "PM";
+    }
+    value1.innerHTML = hours;
+    value2.innerHTML = minutes;
+    value3.innerHTML = seconds;
+    day.innerHTML = day_name[days];
+    date.innerHTML = date_value;
+    ampm.innerHTML = ampm_value;
 }, 1000);
+
